760aea0439e78cc2e39828879b9540ee8146c03c
Dylan McCoy <dmmccoy@clemson.edu>
2017-04-04 16:24:32 -0400

    Traffic code added

--- traffic.js
+++ traffic.js
@@ -22,7 +22,15 @@ var trafficSetup = function() {
 		}
 		else
 		{
-			
+			console.log("TRAFFIC!!!");
+			var trafficURL = "http://dev.virtualearth.net/REST/v1/Traffic/Incidents/"+locationString+"?key=Akl3RNr5drLME8fYbDEpoi--QuIbaK3aIgFZR7oycJ5TdY12QYZMs4D81I9TgzEX"
+			request.get(trafficURL, {
+				json: true
+			},
+			function (error, response, body) {
+				console.log("traffic: " + body.traffic);
+				deviceClient.publish("status", "json", JSON.stringify(body.traffic));
+			});	
 		}
 		++timesGetTrafficCalled;
 	}
